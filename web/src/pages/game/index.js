7392e4b8324f70156272fded8235d31a869a0715
Yusuf Zerdazi <yusuf@zerdazi.com>
2020-06-20 14:12:05 +0100

    improvements

--- web/src/pages/game/index.js
+++ web/src/pages/game/index.js
@@ -45,7 +45,8 @@ class Game extends React.Component {
       ePressed: false,
       aMillis: 0,
       aPressed: false,
-      lastWinner: null
+      lastWinner: null,
+      countdown: ""
     };
 
     this.canvasRef = React.createRef();
@@ -57,16 +58,16 @@ class Game extends React.Component {
   }
 
   onMousedown(e) {
+    var mouseLocked = document.pointerLockElement === this.canvas ||
+      document.mozPointerLockElement === this.canvas;
     if (e.which === 1) {
-      if(document.pointerLockElement === this.canvas ||
-        document.mozPointerLockElement === this.canvas) {
+      if(mouseLocked) {
           this.socket.emit('click');
       } else {
         this.canvas.requestPointerLock();
       }
     } else if (e.which === 3) {
-      if(document.pointerLockElement === this.canvas ||
-        document.mozPointerLockElement === this.canvas) {
+      if(mouseLocked) {
           // right click
       }
     }
@@ -79,7 +80,7 @@ class Game extends React.Component {
 
   componentDidMount() {
     this.mounted = true;
-
+    this.ctx = this.canvasRef.current.getContext("2d");
     store.subscribe(() => {
       var state = store.getState();
       if(this.state.user?.name != state.logIn.user.name){
@@ -108,6 +109,10 @@ class Game extends React.Component {
 
     this.socket.on('winner', (winner) => {
       this.setState({lastWinner: winner});
+    });
+
+    this.socket.on('starting', (timer) => {
+      this.setState({countdown: timer ? timer : ""});
     })
 
     this.socket.on('win', () => {
@@ -118,7 +123,6 @@ class Game extends React.Component {
     });
 
     this.socket.on('kill', () => {
-      console.log("player achieved kill");
       window.PlayFabClientSDK.ExecuteCloudScript({
         FunctionName: "playerKills",
         GeneratePlayStreamEvent: true
@@ -137,8 +141,10 @@ class Game extends React.Component {
     }, 1000 / 60);
 
     document.addEventListener("keydown", e => {
+      if(e.keyCode === 80) {
+        this.setState({fullScreen: this.state.fullScreen ? '' : styles.fullScreen});
+      }
       if(!(document.pointerLockElement === this.canvas || document.mozPointerLockElement === this.canvas)) return;
-      var a;
       if(e.keyCode === 68){
         if(!this.state.ePressed){
           const currentMillis = this.millis();
@@ -165,7 +171,7 @@ class Game extends React.Component {
         e.preventDefault();
         this.socket.emit('space', true);
       }
-      e.keyCode === 83 ? this.socket.emit('down', true) : a = 1;
+      if(e.keyCode === 83) this.socket.emit('down', true);
     });
 
     document.addEventListener("keyup", e => {
@@ -191,7 +197,17 @@ class Game extends React.Component {
 
   draw = () => {
     this.drawBackground();
-    this.state.players.forEach(player => this.drawPlayer(player));
+    this.state.players.filter(p => p.y > 400).forEach(player => this.drawPlayer(player));
+    this.drawPlatform();
+    var players = this.state.players.concat();
+    players.sort((player1, player2) => {
+      if(Math.abs(player2.y - player1.y) > this.state.playerSize){
+        return Math.abs(player2.y) - Math.abs(player1.y);
+      }
+      return Math.abs(player2.x - 480) - Math.abs(player1.x - 480);
+    })
+    players.filter(p => p.y <= 400).forEach(player => this.drawPlayer(player));
+    this.drawStartingTimer();
   }
 
   clearCanvas(){
@@ -199,58 +215,136 @@ class Game extends React.Component {
     ctx.clearRect(0, 0, 960, 540);
   }
 
+  drawPlatform(){
+    var xVanishingPoint = 960 / 2;
+    var yVanishingPoint = -500;
+    
+    this.draw3DSection(0, 600, 0, 0, 480, 410, xVanishingPoint, yVanishingPoint, "grey", false);
+    this.draw3DSection(960, 600, 960, 0, 480, 410, xVanishingPoint, yVanishingPoint, "grey", false);
+    this.draw3DSection(100, 400, 860, 400, 480, 410, xVanishingPoint, yVanishingPoint, "grey", false);
+    
+    this.ctx.beginPath();
+    this.ctx.fillStyle = "black";
+    this.ctx.rect(100, 400, 760, 540);
+    this.ctx.fill();
+  }
+
   drawBackground(){
-    const ctx = this.canvasRef.current.getContext("2d");
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.rect(0, 0, 960, 540);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.fillStyle = "grey";
-    ctx.rect(0, 400, 960, 540);
-    ctx.fill();
+    this.ctx.fillStyle = "white";
+    this.ctx.beginPath();
+    this.ctx.rect(0, 0, 960, 540);
+    this.ctx.fill();
+  }
+
+  draw3DSection(x1, y1, x2, y2, centerX, centerY, xVanishingPoint, yVanishingPoint, colour, useCenterPoint = true, length = 10){
+    var cubeLength = 20;
+
+    var angle1 = Math.atan(((useCenterPoint ? centerY : y1) - yVanishingPoint) / ((useCenterPoint ? centerX : x1) - xVanishingPoint));
+    if((useCenterPoint ? centerX : x1) > xVanishingPoint) angle1 += Math.PI;
+
+    var brPointY = y1 - length;
+    var brPointX = x1 + cubeLength * Math.cos(angle1);
+
+    var angle2 = Math.atan(((useCenterPoint ? centerY : y2) - yVanishingPoint) / ((useCenterPoint ? centerX : x2) - xVanishingPoint));
+    if((useCenterPoint ? centerX : x2) > xVanishingPoint) angle2 += Math.PI;
+
+    var trPointY = y2 - length;
+    var trPointX = x2 + cubeLength * Math.cos(angle2);
+
+    this.ctx.fillStyle = colour;
+    this.ctx.beginPath();
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(brPointX, brPointY);
+    this.ctx.lineTo(trPointX, trPointY);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.closePath();
+    this.ctx.fill();
+  }
+
+  drawPlayerCube(player, width, height, xOffset){
+    var xVanishingPoint = 960 / 2;
+    var yVanishingPoint = -500;
+
+    var rightX= (player.x + xOffset + width);
+    var leftX = (player.x + xOffset);
+    var topY = (player.y - height);
+    var bottomY = (player.y);
+    var centerX= (player.x + xOffset + (width / 2));
+    var centerY = (player.y - (height / 2));
+
+    this.draw3DSection(rightX, bottomY, rightX, topY, centerX, centerY, xVanishingPoint, yVanishingPoint, 'black');
+    this.draw3DSection(leftX, bottomY, leftX, topY, centerX, centerY, xVanishingPoint, yVanishingPoint, 'black');
+    this.draw3DSection(leftX, topY, rightX, topY, centerX, centerY, xVanishingPoint, yVanishingPoint, 'grey');
+
+    this.ctx.beginPath();
+    this.ctx.fillStyle = player.colour;
+    this.ctx.rect(player.x + xOffset, player.y, width, - height);
+    this.ctx.fill();
+  }
+
+  drawPlayerOutline(player, width, height, xOffset){
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = player.colour;
+    this.ctx.rect(player.x + 3 + xOffset, player.y - 3, width - 6, - height + 6);
+    this.ctx.stroke();
+  }
+
+  drawPlayerName(player, height, xOffset){
+    this.ctx.fillStyle = 'white';
+    this.ctx.font = "10px Consolas";
+    this.ctx.textAlign = "center";
+    this.ctx.lineWidth = 6;
+    if(player.name){
+      this.ctx.fillText(player.name, player.x + xOffset + this.state.playerSize / 2, player.y - height - 1);
+    }
+  }
+
+  drawPlayerHealth(player, width, height, xOffset){
+    this.ctx.fillStyle = "black";
+    this.ctx.beginPath();
+    if(player.alive) this.ctx.rect(player.x + width / 2 + xOffset, player.y - height, width / 2, (height * (100 - player.health) / 100));
+    this.ctx.fill();
+  }
+
+  drawPlayerStamina(player, width, height, xOffset){
+    this.ctx.beginPath();
+    this.ctx.fillStyle = 'white';
+    this.ctx.rect(player.x + xOffset, player.y, width / 2, -(height * player.boostCooldown / 100));
+    this.ctx.fill();
   }
 
   drawPlayer(player){
+    // If player is invincible make them flash.
     if(player.alive && player.invincibility != 0 && (Math.round(this.millis() / 10)) % 2 == 0) return;
-    const ctx = this.canvasRef.current.getContext("2d");
-    if(!player.alive) ctx.globalAlpha = 0.3;
+    
+    // If player is dead, make them transparent.
+    if(!player.alive) this.ctx.globalAlpha = 0.3;
+
+
     var currentPlayerHeight = player.ducked ? this.state.playerSize / 5 : this.state.playerSize;
     var currentPlayerWidth = player.ducked ? this.state.playerSize * 1.5 : this.state.playerSize;
     var xOffset = player.ducked ? - 0.25 * this.state.playerSize : 0;
 
-    ctx.beginPath();
-    ctx.fillStyle = player.colour;
-    ctx.rect(player.x + xOffset, player.y, currentPlayerWidth, - currentPlayerHeight);
-    ctx.fill();
+    this.drawPlayerCube(player, currentPlayerWidth, currentPlayerHeight, xOffset);
+    this.drawPlayerName(player, currentPlayerHeight, xOffset);
 
-    ctx.fillStyle = player.colour;
-    ctx.lineWidth = 6;
-    if(player.name){
-      ctx.fillText(player.name, player.x + xOffset, player.y - currentPlayerHeight - 3);
-    }
-
+    // If player is dead, don't draw the rest.
     if(!player.alive){
-      ctx.globalAlpha = 1;
+      this.ctx.globalAlpha = 1;
       return;
     }
 
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    if(player.alive) ctx.rect(player.x + currentPlayerWidth / 2 + xOffset, player.y - currentPlayerHeight, currentPlayerWidth / 2, (currentPlayerHeight * (100 - player.health) / 100));
-    ctx.fill();
+    this.drawPlayerHealth(player, currentPlayerWidth, currentPlayerHeight, xOffset);
+    this.drawPlayerStamina(player, currentPlayerWidth, currentPlayerHeight, xOffset);
+    this.drawPlayerOutline(player, currentPlayerWidth, currentPlayerHeight, xOffset);
+    this.ctx.globalAlpha = 1;
+  }
 
-    ctx.beginPath();
-    ctx.fillStyle = 'white';
-    ctx.rect(player.x + xOffset, player.y, currentPlayerWidth / 2, -(currentPlayerHeight * player.boostCooldown / 100));
-    ctx.fill();
-    
-    ctx.beginPath();
-    ctx.strokeStyle = player.colour;
-    ctx.rect(player.x + 3 + xOffset, player.y - 3, currentPlayerWidth - 6, - currentPlayerHeight + 6);
-    ctx.stroke();
-    
-    ctx.globalAlpha = 1;
+  drawStartingTimer(){
+    this.ctx.fillStyle = 'black';
+    this.ctx.font = "50px Consolas";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText(this.state.countdown, 480, 270);
   }
 
   playSound(soundFile){
@@ -313,11 +407,11 @@ class Game extends React.Component {
     </div>;
     return (
       <>
-        <canvas ref={this.canvasRef} width={960} height={540} />
+        <canvas className={this.state.fullScreen} ref={this.canvasRef} width={960} height={540} />
         <div className={styles.addAi}>
           <span className={this.state.nameClass}><input onChange={this.handleChange} placeholder="Enter name" className={this.state.nameInputClass} type="text"></input></span>
           {this.props.user?.name ? <span className={styles.playFabName}><b>Name:</b> {this.props.user?.name}</span> : <></> }
-          <span onClick={this.setName} className={styles.addAiButton}>Set Name</span>
+          {this.props.user ? <span onClick={this.setName} className={styles.addAiButton}>Update Username</span> : <></> }
           <span onClick={this.play} className={styles.addAiButton + ' ' + styles.playButton}>Play</span>
           <span onClick={this.quit} className={styles.addAiButton + ' ' + styles.quitButton}>Quit</span>
           <span onClick={this.addAi} className={styles.addAiButton}>+AI</span>
@@ -337,6 +431,7 @@ class Game extends React.Component {
           <span className={styles.control}><b>D:</b> Right (Double tap to boost)</span>
           <span className={styles.control}><b>Space:</b> Jump/Double jump</span>
           <span className={styles.control}><b>Click:</b> Boost</span>
+          <span className={styles.control}><b>P:</b> Toggle Fullscreen</span>
         </div>
       </>
     );
