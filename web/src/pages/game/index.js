648164c8b05005f9a508041a0ea668fb11d8eecb
Yusuf Zerdazi <yusuf@zerdazi.com>
2020-06-25 23:07:43 +0100

    Use guid custom ID

--- web/src/pages/game/index.js
+++ web/src/pages/game/index.js
@@ -23,6 +23,13 @@ const mapDispatchToProps = dispatch => ({
   })
 });
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 class Game extends React.Component {
   constructor(props){
     super(props);
@@ -200,7 +207,9 @@ class Game extends React.Component {
         this.socket.emit('left', true)
       }
       if(e.keyCode === 32 || e.keyCode == 87){
-        e.preventDefault();
+        if(e.keyCode == 32){
+          e.preventDefault();
+        }
         this.socket.emit('space', true);
       }
       if(e.keyCode === 83) this.socket.emit('down', true);
@@ -420,10 +429,14 @@ class Game extends React.Component {
 
   play(){
     if(this.state.name && !this.props.user?.name){
+      var customId = localStorage.getItem("customId");
+      if(!customId){
+        customId = uuidv4();
+      }
       window.PlayFabClientSDK.LoginWithCustomID({
         CreateAccount : true,
         TitleId: "B15E8",
-        CustomId: this.state.name
+        CustomId: customId
       }, () => {
         this.setName();
       });
