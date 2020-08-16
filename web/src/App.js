13b6e6c2b79be1c4fcffc3a6c6d8cabad24ad024
Yusuf Zerdazi <yusuf@zerdazi.com>
2020-08-16 19:33:16 +0100

    Fix routes

--- web/src/App.js
+++ web/src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
         <Router>
           <Header/>
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Game/>
             </Route>
             <Route exact path="/leaderboard">
