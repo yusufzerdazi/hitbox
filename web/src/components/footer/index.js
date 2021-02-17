55f622fe8a2b2c6720fce567843f46fb3293cdb3
Yusuf Zerdazi <yusuf@zerdazi.com>
2021-02-17 19:34:11 +0000

    More improvements and animations

--- web/src/components/footer/index.js
+++ web/src/components/footer/index.js
@@ -1,14 +1,24 @@
 
 import React from 'react';
 import yz from '../../assets/images/yz.svg';
+import cashapp from '../../assets/images/cashapp.svg';
 import styles from './styles.module.css';
+import Tooltip from 'react-bootstrap/Tooltip'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 class Footer extends React.Component {
   render() {
     return (
       <div className={styles.footerContainer}>
         <a href="https://yusuf.zerdazi.com">
-          <img alt="Yusuf Zerdazi" className={styles.footerImage} src={yz}></img>
+          <OverlayTrigger placement="top" overlay={<Tooltip id={`website`}>My website</Tooltip> }>
+            <img alt="Yusuf Zerdazi" className={styles.footerImage} src={yz}></img>
+          </OverlayTrigger>
+        </a>
+        <a href="https://cash.app/£yusufzerdazi">
+          <OverlayTrigger placement="top" overlay={<Tooltip id={`donate`}>Donate</Tooltip> }>
+            <img alt="Donate" className={styles.footerImage} src={cashapp}></img>
+          </OverlayTrigger>
         </a>
       </div>
     );
