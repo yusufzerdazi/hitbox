8b6304f33a4dad891c8b06386feb372fe98bde5c
Yusuf Zerdazi <yusuf@zerdazi.com>
2023-08-31 13:24:58 +0100

    path

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -9,7 +9,7 @@ const os = require('os');
 module.exports = {
   apps : [{
     name: "colyseus-app",
-    script: 'lib/index.js',
+    script: 'build/index.js',
     time: true,
     watch: false,
     instances: os.cpus().length,
