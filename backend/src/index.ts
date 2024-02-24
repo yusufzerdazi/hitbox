40cf4060a3badc59734ea78c1ff7c6a4fe0bd8a0
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-02-24 20:05:25 +0000

    Packages update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,8 +8,8 @@
  */
 import { listen } from "@colyseus/tools";
 
-// Import arena config
-import arenaConfig from "./app.config";
+// Import Colyseus config
+import app from "./app.config";
 
 // Create and listen on 2567 (or PORT environment variable.)
-listen(arenaConfig);
+listen(app);
