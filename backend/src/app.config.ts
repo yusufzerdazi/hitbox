634522c288270d39d0c7eab838eb048b307a4e93
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-08-30 20:35:16 +0100

    Use connection string

--- backend/src/app.config.ts
+++ backend/src/app.config.ts
@@ -6,7 +6,7 @@ import { GameRoom } from './rooms/gameRoom';
 import { PlayFabServer } from 'playfab-sdk';
 
 let appInsights = require("applicationinsights");
-appInsights.setup("c21377b2-7f52-4b26-b54c-69916d75e54c")
+appInsights.setup("InstrumentationKey=c21377b2-7f52-4b26-b54c-69916d75e54c;IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=19ec08fa-f5a4-4239-9195-375d5085bcc1")
     .start();
 
 PlayFabServer.settings.titleId = '5B7C3';
